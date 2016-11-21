1f3fa622258d199632a509966c9712951d4869f0
Tai Chen <mindlikewater@gmail.com>
2016-11-21 15:45:01 -0500

    Added a ContactService to the API

---
+++ src/js/services/contact-server.js
@@ -0,0 +1,20 @@
+import { SERVER } from "../server";
+
+function GifService ($http) {
+  this.allContacts = function () {
+    return $http.get(SERVER);
+  };
+
+  this.addContact = function (person) {
+    return $http.post(SERVER, person);
+  };
+
+  this.getContact = function (id) {
+    let url = SERVER + id;
+    return $http.get(url);
+  };
+
+};
+
+ContactService.$inject = ['$http'];
+export { ContactService };
