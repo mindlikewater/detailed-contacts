abc6537b5e9f4dc7c0155e2b56ea320de59cdc4e
Tai Chen <mindlikewater@gmail.com>
2016-11-21 14:55:12 -0500

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+
+import { routerConfig } from './routes';
+import { HomeController } from './controllers/home';
+import { AddController } from './controllers/add';
+import { SingleController } from './controllers/single';
+import { LayoutController } from './controllers/layout';
+import { ContactService } from './services/contact-server';
+
+import 'angular-ui-router';
+
+angular
+  .module('app', ['ui.router'])
+  .config(routerConfig)
+  .controller('LayoutController', LayoutController)
+  .controller('HomeController', HomeController)
+  .controller('AddController', AddController)
+  .controller('SingleController', SingleController)
+  .service('ContactService', ContactService);
