ddc643ab0553994c21caeb42f4d310c03487d89c
Tai Chen <mindlikewater@gmail.com>
2016-11-21 15:14:04 -0500

    Added basic routes for home view, adding contacts, and single contacts

---
+++ src/js/routes.js
@@ -0,0 +1,28 @@
+function routerConfig ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      templateUrl: 'templates/layout.tpl.html',
+      controller: 'LayoutController as layout'
+    })
+    .state('root.home', {
+      url: '/',
+      templateUrl: 'templates/home.tpl.html',
+      controller: 'HomeController as home'
+    })
+    .state('root.add', {
+      url: '/add',
+      templateUrl: 'templates/add.tpl.html',
+      controller: 'AddController as add'
+    })
+    .state('root.single', {
+      url: '/contacts/:id',
+      templateUrl: 'templates/single.tpl.html',
+      controller: 'SingleController as single'
+    });
+
+  $urlRouterProvider.otherwise('/');
+};
+
+routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+export { routerConfig };
