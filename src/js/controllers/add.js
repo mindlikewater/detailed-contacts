8b6723fe2bbb5fb3faa1045ecbded13543c88896
Tai Chen <mindlikewater@gmail.com>
2016-11-22 19:42:31 -0500

    Added method for getting a new contact

--- src/js/controllers/add.js
+++ src/js/controllers/add.js
@@ -1,2 +1,15 @@
-AddController.$inject = ['$state', 'GifService'];
+function AddController ($state, ContactService) {
+  let vm = this;
+
+  this.addContact = addContact;
+
+  function addContact (contact) {
+    console.log("Testing add controller");
+    ContactService.addContact(contact).then((resp) => {
+      $state.go('root.home');
+    });
+  };
+};
+
+AddController.$inject = ['$state', 'ContactService'];
 export { AddController };
