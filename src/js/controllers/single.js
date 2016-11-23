f04e5d17600356d2c21a58439d636b50d1578c48
Tai Chen <mindlikewater@gmail.com>
2016-11-22 19:51:06 -0500

    Added method for getting a single contact

--- src/js/controllers/single.js
+++ src/js/controllers/single.js
@@ -1,2 +1,16 @@
-SingleController.$inject = ['GifService', '$stateParams'];
+function SingleController (ContactService, $stateParams) {
+  let vm = this;
+
+  vm.contact = {};
+
+  function init () {
+    ContactService.getContact($stateParams.id).then((resp) => {
+      vm.contact = resp.data;
+    });
+  };
+
+  init();
+}
+
+SingleController.$inject = ['ContactService', '$stateParams'];
 export { SingleController };
